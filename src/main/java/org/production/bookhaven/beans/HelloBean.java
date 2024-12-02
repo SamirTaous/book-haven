0a12a30337769b9e338238aa93f06a38e5b4d651
SamirTaous <hgta626@gmail.com>
2024-12-02 20:48:56 +0100

    Added dependencies & Tested Wildfly Server

---
+++ src/main/java/org/production/bookhaven/beans/HelloBean.java
@@ -0,0 +1,31 @@
+package org.production.bookhaven.beans;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class HelloBean {
+    private String name;
+    private String greeting;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public void sayHello() {
+        this.greeting = "Hello, " + name + "!";
+    }
+}
---
+++ src/main/java/org/production/bookhaven/models/Customer.java
@@ -0,0 +1,8 @@
+package org.production.bookhaven.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Customer {
+
+}
